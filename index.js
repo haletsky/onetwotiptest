dc19ba18ea29ef923bb0aae0e2a754b0873f958d
haletsky <bogdan.haletsky@gmail.com>
2017-12-05 18:01:54 +0200

    Added --get-list functionality.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const argv = require('minimist')(process.argv.slice(2))
 const redis = require('redis')
 
-if (argv['get-error']) {
-  // TODO: console out all errors from redis and clean up database
-}
-
 const port = argv.port || 6379
 const host = argv.host || '127.0.0.1'
 const isGenerator = argv.generator
@@ -19,59 +15,70 @@ const client = redis.createClient({
   port
 })
 
-function generatorLogic () {
-  let messageCount = 0
+if (argv['get-errors']) {
+  client.lrange(errorList, 0, -1, (err, list) => {
+    if (!err) console.log(list)
+    else console.error(err)
+    client.del(errorList, err => {
+      if (!err) {
+        client.quit()
+        process.exit(0)
+      } else console.log(err)
+    })
+  })
+} else {
+  function generatorLogic () {
+    let messageCount = 0
 
-  const generate = () => {
-    console.log('emit message:', messageCount)
-    client.publish(channels.messages, messageCount++)
+    const generate = () => {
+      console.log('emit message:', messageCount)
+      client.publish(channels.messages, messageCount++)
+    }
+
+    setInterval(generate, 500)
   }
 
-  setInterval(generate, 500)
-}
+  function observerLogic () {
+    client.on('subscribe', (channel, message) => {
+    })
 
-function observerLogic () {
-  client.on('subscribe', (channel, message) => {
-  })
-
-  client.on('message', (channel, message) => {
-    // TODO: receive message, simulate the work, and probably crash
-
-    switch (channel) {
-      case channels.messages:
-        const eventHandler = (msg, callback) => {
-          const onComplete = () => {
-            var error = Math.random() > 0.85
-            callback(error, msg)
+    client.on('message', (channel, message) => {
+      switch (channel) {
+        case channels.messages:
+          const eventHandler = (msg, callback) => {
+            const onComplete = () => {
+              var error = Math.random() > 0.85
+              callback(error, msg)
+            }
+            // processing takes time...
+            setTimeout(onComplete, Math.floor(Math.random() * 1000))
           }
-          // processing takes time...
-          setTimeout(onComplete, Math.floor(Math.random() * 1000))
-        }
 
-        eventHandler(message, (err, msg) => {
-          if (err) {
-            client.unsubscribe(channels.messages, err => {
-              if (!err) {
-                client.send_command('lpush', [errorList, msg], (err, msg) => {
-                  if (!err) {
-                    client.subscribe(channels.messages)
-                  }
-                })
-              }
-            })
-          }
-        })
-        break
-      case channels.system:
-        console.log('SYSTEM:', message)
-        break
-      default:
-        console.error('Undefined channel.')
-    }
-  })
+          eventHandler(message, (err, msg) => {
+            if (err) {
+              client.unsubscribe(channels.messages, err => {
+                if (!err) {
+                  client.send_command('lpush', [errorList, msg], err => {
+                    if (!err) {
+                      client.subscribe(channels.messages)
+                    } else console.error('senc_command ERROR:', err)
+                  })
+                } else console.error('Unsubscribe ERROR:', err)
+              })
+            } else console.error('MESSAGES:', msg)
+          })
+          break
+        case channels.system:
+          console.log('SYSTEM:', message)
+          break
+        default:
+          console.error('Undefined channel.')
+      }
+    })
 
-  client.subscribe(channels.messages)
+    client.subscribe(channels.messages)
+  }
+
+  if (isGenerator) generatorLogic()
+  else observerLogic()
 }
-
-if (isGenerator) generatorLogic()
-else observerLogic()
